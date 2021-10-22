c5200ba57b3ce305467b3fb89847e0e853d10306
Dayu Indira <indiradayu13@gmail.com>
2021-10-22 11:58:57 +0700

    Challange 5

---
+++ Main.java
@@ -0,0 +1,38 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("I A Indira Herdasari", 19, 164);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      
+      hero.lari();
+
+      hero.makan();
+      hero.makan();
+      hero.makan();
+      hero.makan();
+      hero.makan();
+
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
